a8786e2685466e17f3b7cc7d232513b3e27baf78
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-03-01 21:26:47 +0530

    route develope

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function createSampleData() {
     console.log("Lead created:", lead);
     return lead;
   } catch (error) {
-    console.error("Erro cri led:", error);
+    console.error("Erro cr led:", error);
     throw error;
   }
 }
