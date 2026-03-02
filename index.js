4877db5ff5af6781b3db9837a674a51d5d878fd3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-03-02 11:51:56 +0530

    route develope

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const { initializeDatabase } = require("./config/db");
